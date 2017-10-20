57abe9c99f834ef54445751feafce405073cddbd
Angular CLI <angular-cli@angular.io>
2017-10-19 21:35:10 -0400

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('cpha-notification-clinician App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
